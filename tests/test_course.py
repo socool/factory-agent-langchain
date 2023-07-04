93fac9847297a7ad811790800af79885a0146d06
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2023-07-04 16:40:37 +0700

    add class test course

---
+++ tests/test_course.py
@@ -0,0 +1,15 @@
+from factory_agent_langchain.factory_agent_langchain import DSA, construct_course
+
+
+def test_import() -> None:
+    import factory_agent_langchain  # noqa: F401
+
+    assert "Empty test to ensure pytest passes."
+
+def test_dsa() -> None:
+    import factory_agent_langchain  # noqa: F401
+    dsa = DSA()
+    create_course = construct_course(DSA)
+    print(create_course)
+    assert create_course.fee == 8000
+    assert create_course.batches == 5
